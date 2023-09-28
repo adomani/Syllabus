92c82216025e836ccd17d26866fd9c51ed105f3a
adomani <adomani@gmail.com>
2023-09-28 15:54:11 +0200

    Define scatterTPwL function

--- .src/buildMA4N1.sh
+++ .src/buildMA4N1.sh
@@ -9,35 +9,37 @@ mergeFiles () {
   done > source.md
 }
 
-awk -v home="${HOME}/${matONo}/Warwick/Syllabus/MA4N1/" 'BEGIN {
-  mainFileNoExt="toc"
-  mainFile=mainFileNoExt".md"
-  genericGHurl="https://adomani.github.io/Syllabus/MA4N1/"
-  ghurl=genericGHurl mainFileNoExt
-  toc="\n\n## Available pages\n\n* [Current syllabus](https://adomani.github.io/Syllabus/MA4N1)\n"
-  moodleurl="https://moodle.warwick.ac.uk/course/view.php?id=58287#section-0"
-  tpwlink="\n\n[Back to the `Theorem Proving with Lean` webpage](" ghurl ")"
-  moodlink="\n\n[Back to Moodle](" moodleurl ")"
-}
-  /^<!-- newFile [^ ]* -->/  { curr=$3; content[curr]=""; fileNames[$3]++; con=1; }
-  con == 1 && /^##* / { toc=toc sprintf("* [%s](%s%s)\n", $0, genericGHurl, curr) }
-  !/^<!-- newFile [^ ]* -->/ { con=0; content[curr]=content[curr] $0"\n" }
- END {
-  gsub(/\[##*  */, "[", toc)
-  gsub(/\.md)/, ")", toc)
-  print toc
-  mainFound=0
-  for (fil in content) {  ## fil is the name of the file, content[fil] is its content
-    if (fil == mainFile) { mainFound++; lk=toc; printf("* "); } else { lk=tpwlink; printf "  "}
-    print fil
-    path=home fil
-    printf("%s\n---%s%s\n", content[fil], lk, moodlink) > path
-    close(path)
+scatterTPwL () {
+  awk -v home="${HOME}/${matONo}/Warwick/Syllabus/MA4N1/" 'BEGIN {
+    mainFileNoExt="toc"
+    mainFile=mainFileNoExt".md"
+    genericGHurl="https://adomani.github.io/Syllabus/MA4N1/"
+    ghurl=genericGHurl mainFileNoExt
+    toc="\n\n## Available pages\n\n* [Current syllabus](https://adomani.github.io/Syllabus/MA4N1)\n"
+    moodleurl="https://moodle.warwick.ac.uk/course/view.php?id=58287#section-0"
+    tpwlink="\n\n[Back to the `Theorem Proving with Lean` webpage](" ghurl ")"
+    moodlink="\n\n[Back to Moodle](" moodleurl ")"
   }
-  if (mainFound != 1) printf("\n** The main file `%s` was not declared! **\n", mainFile)
-  for (name in fileNames)
-    if (fileNames[name] >= 2) printf("\n** The file `%s` was declared %s times! **\n", name, fileNames[name])
-}' ~/Matematica/Warwick/Syllabus/MA4N1/source.md
+    /^<!-- newFile [^ ]* -->/  { curr=$3; content[curr]=""; fileNames[$3]++; con=1; }
+    con == 1 && /^##* / { toc=toc sprintf("* [%s](%s%s)\n", $0, genericGHurl, curr) }
+    !/^<!-- newFile [^ ]* -->/ { con=0; content[curr]=content[curr] $0"\n" }
+   END {
+    gsub(/\[##*  */, "[", toc)
+    gsub(/\.md)/, ")", toc)
+#    print toc
+    mainFound=0
+    for (fil in content) {  ## fil is the name of the file, content[fil] is its content
+      if (fil == mainFile) { mainFound++; lk=toc; printf("* "); } else { lk=tpwlink; printf "  "}
+      print fil
+      path=home fil
+      printf("%s\n---%s%s\n", content[fil], lk, moodlink) > path
+      close(path)
+    }
+    if (mainFound != 1) printf("\n** The main file `%s` was not declared! **\n", mainFile)
+    for (name in fileNames)
+      if (fileNames[name] >= 2) printf("\n** The file `%s` was declared %s times! **\n", name, fileNames[name])
+  }' ~/Matematica/Warwick/Syllabus/.src/TPwL_source.md
+}
 
 
 
